d90357b058f3a25679b99aadef33807cfeb39fb5
Ganesh Nadar <gan1234568@gmail.com>
2021-05-02 23:07:16 +0530

    Adding outputs

---
+++ terraform/outputs.tf
@@ -0,0 +1,11 @@
+output "cluster_name" {
+  value = "${module.gke.cluster_name}"
+}
+
+output "cluster_zone" {
+  value = "${module.gke.cluster_zone}"
+}
+
+output "image_url" {
+  value = "${module.gke.image_url}"
+}
