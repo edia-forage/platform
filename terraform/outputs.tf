c5d77ebdb9ffde9eaa49619a24d4904c2ad5e440
Ganesh Nadar <gan1234568@gmail.com>
2021-05-03 19:53:54 +0530

    Enable IPv4 alias

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,9 +2,9 @@ output "cluster_name" {
   value = "${module.gke.cluster_name}"
 }
 
-// output "cluster_zone" {
-//   value = "${module.gke.cluster_zone}"
-// }
+output "cluster_zone" {
+  value = "${module.gke.cluster_zone}"
+}
 
 output "image_url" {
   value = "${module.gke.image_url}"
