3507ae875d9b28ecadb29d62da5c6c8eb903a421
Ganesh Nadar <gan1234568@gmail.com>
2021-05-03 09:54:24 +0530

    Adding outputs for Cloud SQL module

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -13,3 +13,20 @@ output "image_url" {
 output "sql_connection_name" {
   value = "${module.cloud_sql.sql_connection_name}"
 }
+
+output "db_name" {
+  value = "${module.cloud_sql.db_name}"
+}
+
+output "host" {
+  value = "${module.cloud_sql.host}"
+}
+
+output "db_username" {
+  value = "${module.cloud_sql.username}"
+}
+
+output "db_password" {
+  value     = "${module.cloud_sql.password}"
+  sensitive = true
+}
